5fe64b3a292c46a15d0c4d5ddba847891ad9e949
Kevin Nguyen <Keng3875@colorado.edu>
2017-05-03 13:56:23 -0600

    DB configuration

---
+++ dbconfig.php
@@ -0,0 +1,9 @@
+  <? php
+    $host = "localhost";
+    $user = "root";
+    $password = "Theanswer14";
+    $database = "project";
+ 
+mysql_connect($host,$user,$password);
+mysql_connect($database);
+?>
