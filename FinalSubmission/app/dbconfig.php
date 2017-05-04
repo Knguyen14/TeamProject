fca51af5fa3bf80510ca6c5be7416e8483fb2c37
princeantwi <pran4690@colorado.edu>
2017-05-04 14:52:12 -0600

    Add files via upload

---
+++ FinalSubmission/app/dbconfig.php
@@ -0,0 +1,9 @@
+  <? php
+    $host = "localhost";
+    $user = "newuser";
+    $password = "password";
+    $database = "project";
+ 
+mysql_connect($host,$user,$password);
+mysql_connect($database);
+?>
