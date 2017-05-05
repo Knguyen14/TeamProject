ecc2492a1639561c1c25fecc4d7201dc0523ca27
Decker-Russell <rude5139@colorado.edu>
2017-05-04 22:24:57 -0600

    may have accidentally deleted som stuff

--- FinalSubmission/html/dbconfig.php
+++
@@ -1,9 +0,0 @@
-  <? php
-    $host = "localhost";
-    $user = "newuser";
-    $password = "password";
-    $database = "project";
- 
-mysql_connect($host,$user,$password);
-mysql_connect($database);
-?>
