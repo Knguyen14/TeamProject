7485278bc7494b5750296eeaa74429b07015f532
Decker-Russell <rude5139@colorado.edu>
2017-05-04 21:12:06 -0600

    auto doc

---
+++ apidoc/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "mapapp",
+  "version": "1.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-05-05T03:08:18.430Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.5"
+  }
+});
